f49373445c556b3e37acec5e2ce6f1b18b241c7c
abhimanyu012 <abhimanyucode2021@gmail.com>
2025-10-31 23:12:20 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const PORT = 5000;
+app.get("/",(req,res)=>{
+    res.json({
+        
+    })
+})
+app.listen(prompt, console.log(`server is running on ${PORT}`));
